98bfcff7ae14b6a1b05bf29b7ea859c3123d52f0
H Heim <hheim8@gmail.com>
2012-06-18 20:00:46 -0500

    r006
    Fixed the random-number generator. Switched up the "main" method. Added
    a "switch" method to control setting the boxes to their respective
    words.

--- practice-hangman/practice-hangman/Form1.cs
+++ practice-hangman/practice-hangman/Form1.cs
@@ -17,12 +17,14 @@ namespace WindowsFormsApplication1
         }
 
         //A way for making a random number based off of System Time. -h3
-        void randomInt(int i1,int i2)
+        int randomInt(int i1,int i2)
         {
             int wordSeed;
 
             System.Random RandNum = new System.Random();
             wordSeed = RandNum.Next(i1, i2);
+
+            return wordSeed;
         }
 
         //This is an easy way to update the answer box. Use like this: 'setEqual(c1, c2, c3, c4, c5);' -h3
@@ -43,13 +45,132 @@ namespace WindowsFormsApplication1
 
         void main()
         {
+            int random;
+
+            pictureBox.Image = WindowsFormsApplication1.Properties.Resources.zeroW; //Setting the picturebox to the "zerowrong" image -h3
+
+            random = randomInt(1, 10); //Getting a random number for the word -h3
+
+            wordSwitch(random); //Sending the word-choosing somewhere else because that's one giant switch statement -h3
+            //-*-*-*-If anyone has a better way of doing this please please share it-*-*-*-
+        }
+
+        void wordSwitch(int switchInt)
+        {
             char c1;
             char c2;
             char c3;
             char c4;
             char c5;
 
-            pictureBox.Image = WindowsFormsApplication1.Properties.Resources.zeroW;
+            switch (switchInt)
+            {
+                case 1:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '1';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 2:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '2';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 3:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '3';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 4:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '4';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 5:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '5';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 6:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '6';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 7:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '7';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 8:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '8';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                case 9:
+                    {
+                        c1 = 'W';
+                        c2 = 'O';
+                        c3 = 'R';
+                        c4 = 'D';
+                        c5 = '9';
+                        setEqual(c1, c2, c3, c4, c5);
+
+                        break;
+                    }
+                default:
+                    {
+                        MessageBox.Show("Something has gone wrong. Contact us somehow.", "ERROR");
+
+                        break;
+                    }
+            }
         }
     }
 }
